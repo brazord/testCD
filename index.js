87f1d581141f08eab036e50b9db4cc277f351f3f
rodrigo rodrigues <rrodrigues@server21.local>
2025-04-10 21:18:19 -0300

    bugfix: correcao divisao

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ console.log("Hello, GitHub Actions");
 
 function division(a, b) {
     if (b === 0) {
-      //  throw new Error("Divisão por zero não é permitida");
+       throw new Error("Divisão por zero não é permitida");
       console.log('minha nova implementação');
     }
     return a / b;
